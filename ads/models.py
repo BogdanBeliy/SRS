c75822e07523eca860977089b7412afde66268ec
korsun <korsun.da@intechs.by>
2022-01-19 15:41:44 +0300

    firs part ads models

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
+from core.abstractions.model_abstract import CreationDateAbstract
 
-# Create your models here.
+
+class Category(CreationDateAbstract):
+    """ Category model """
+    name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True, verbose_name='Описание')
+
+
+class SubCategory(CreationDateAbstract):
+    """ SubCategory model """
+    name = models.CharField(max_length=255, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_category')
+
+
+class Advertisement(CreationDateAbstract):
+    """ Advertisement model """
+    name = models.CharField(max_length=255, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='advertisement')
+    photo = models.ImageField(blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True, verbose_name='Описание объявления')
+
+
+
+
