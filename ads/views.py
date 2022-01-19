7fb398d4ee20cf60f9cb04272359d5540754fd8b
korsun <korsun.da@intechs.by>
2022-01-19 18:25:38 +0300

    add ShowAllAdvertisementView (ads/views.py) add url 'all-advertisement'

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 
-from ads.models import *
+from ads.models import Category, SubCategory, Advertisement
 from ads.serializers import *
 from rest_framework.views import APIView
 
@@ -17,5 +17,13 @@ class AllCategoriesView(APIView):
         ...
 
 
+class ShowAllAdvertisementView(APIView):
+    serializer_class = AdvertisementSerializer
 
+    def get(self, request, *args, **kwargs):
+        advertisement = Advertisement.objects.all()
+        serializer = self.serializer_class(advertisement, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, *args, **kwargs):
+        ...
