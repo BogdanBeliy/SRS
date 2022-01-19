e4b5be3eb8478e45b81ffebc225a595777e8144b
korsun <korsun.da@intechs.by>
2022-01-19 16:28:26 +0300

    fix ads/models

--- ads/models.py
+++ ads/models.py
@@ -17,10 +17,20 @@ class SubCategory(CreationDateAbstract):
 
 class Advertisement(CreationDateAbstract):
     """ Advertisement model """
+
+    TYPE_ADVERTISEMENT = (
+        ('free', 'free'),
+        ('paid', 'paid'),
+    )
     name = models.CharField(max_length=255, blank=True, null=True)
+    type_advertisement = models.CharField(max_length=5, choices=TYPE_ADVERTISEMENT, default='free')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='advertisement')
     photo = models.ImageField(blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True, verbose_name='Описание объявления')
+    cost = models.DecimalField(blank=True, null=True)
+    additional_equipment = models.JSONField(blank=True, null=True, verbose_name='Дополнительное оборудование')
+    characteristic_vehicle = models.JSONField(blank=True, null=True, verbose_name='Характеристики техники')
+
 
 
 
