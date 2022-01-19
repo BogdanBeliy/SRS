ba5d38265bdc52b4691e18853fbee2531a7415db
korsun <korsun.da@intechs.by>
2022-01-19 17:11:30 +0300

    add admin.py (ads/admin.py)

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from ads.models import *
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name', 'category')
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'payment_type')
+    search_fields = ('name', 'category')
+    list_filter = ('category', 'cost', 'payment_type')
+
