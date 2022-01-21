3381c298702150c2f7393349381984c3fb25ce9c
bogdan <belogur.bi@intechs.by>
2022-01-21 17:23:47 +0300

    add  customUserSerializer

--- account/views.py
+++ account/views.py
@@ -5,10 +5,18 @@ from account.serializers import CustomUserSerializer
 from rest_framework.views import APIView
 
 
-class AllUserView(APIView):
+class UserView(APIView):
     serializer_class = CustomUserSerializer
 
     def get(self, request, *args, **kwargs):
-        users = CustomUser.objects.all()
-        serializer = self.serializer_class(users, many=True).data
+        users = CustomUser.objects.get(id=kwargs['user_id'])
+        serializer = self.serializer_class(users).data
         return Response(serializer)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=self.request.data)
+        serializer.is_valid()
+        print(serializer.data)
+        return Response('okey')
+
+
