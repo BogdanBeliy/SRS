2aa9193ee391791960ac890c80b2883b2aafbbb9
bogdan <belogur.bi@intechs.by>
2022-02-16 15:09:36 +0300

    add django-silk lib add detail adv view add ads by cat view

--- ads/urls.py
+++ ads/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from ads.views import *
+from ads.views import ShowAllCategoriesView, AdvByCatView, AdvDetailView
 
 urlpatterns = [
     path('categories/', ShowAllCategoriesView.as_view(), name='all_categories'),
+    path('by-category/', AdvByCatView.as_view(), name='adv_by_cat'),
+    path('detail/', AdvDetailView.as_view(), name='detail'),
 ]
